bcf66cefcf29ded2fc72f7396ab4a94fd256ab44
Karen <kpl.lau@gmail.com>
2022-02-03 21:32:48 -0800

    add terrain walls

--- src/level_manager.cpp
+++ src/level_manager.cpp
@@ -47,13 +47,21 @@ void LevelManager::load_level(int level)
     if (level == 0) {
         Entity enemy = createEnemy(vec2(600, 500), vec2(50, 100));
         Entity player = createPlayer(vec2(500, 500), vec2(50, 100));
-		Entity button = createButton(vec2(100, 300), vec2(50, 50), mock_callback);
+		Entity button = createButton(vec2(100, 400), vec2(50, 50), mock_callback);
 
-        float terrain_offset = 0.f;
-        while (terrain_offset < 1200.f) {
-            Entity curr = createTerrain(vec2(100.001 + terrain_offset, 600), vec2(100, 100));
+        float terrain_x_offset = 0.f;
+        while (terrain_x_offset < 1200.f) {
+            Entity curr = createTerrain(vec2(100.001 + terrain_x_offset, 600.001), vec2(100, 100));
             terrain_vector.push_back(curr);
-            terrain_offset += 100.001;
+            terrain_x_offset += 100.001;
+        }
+        float terrain_y_offset = 100.001;
+        while (terrain_y_offset < 200.f) {
+            Entity curr1 = createTerrain(vec2(100.001, 600.001 - terrain_y_offset), vec2(100, 100));
+            Entity curr2 = createTerrain(vec2(1200.001, 600.001 - terrain_y_offset), vec2(100, 100));
+            terrain_vector.push_back(curr1);
+            terrain_vector.push_back(curr2);
+            terrain_y_offset += 100.001;
         }
 
         order_vector.push_back(enemy);
