687424b604a3d0bcbb9d9409a6346c88140b7664
John Shin <shinhs0506@gmail.com>
2022-02-20 14:46:27 -0800

    update createButton in level_init to use createGenericButton in game_init

--- src/level_init.cpp
+++ src/level_init.cpp
@@ -1,5 +1,6 @@
 #include "level_init.hpp"
 #include "tiny_ecs_registry.hpp"
+#include "game_init.hpp"
 
 Entity createDebugLine(vec2 position, vec2 scale)
 {
@@ -217,20 +218,7 @@ void removeCamera(Entity entity)
 
 Entity createButton(vec2 pos, vec2 size, void (*on_click)())
 {
-	auto entity = Entity();
-
-	Motion& motion = registry.motions.emplace(entity);
-	motion.position = pos;
-	motion.prev_position = pos;
-	motion.angle = 0.f;
-	motion.velocity = { 0.f, 0.f };
-	motion.scale = size;
-
-	Clickable clickable{ on_click };
-	registry.clickables.insert(entity, clickable);
-
-	Overlay overlay{pos};
-	registry.overlays.insert(entity, overlay);
+	auto entity = createGenericButton(pos, size, on_click);
 
 	registry.renderRequests.insert(
 		entity,
