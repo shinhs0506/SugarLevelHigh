3f908185be7cad9a05270b62a2f9498871577e91
John Shin <shinhs0506@gmail.com>
2022-03-19 20:41:18 -0700

    removed dot init

--- src/ability_list.hpp
+++ src/ability_list.hpp
@@ -9,57 +9,57 @@ abilities and attack from there. (boxes not shown/active during enemy turn)
 */
 
 AttackAbility gingerbread_basic_attack = {
-    .activated = false,
-    .speed = 0,
-    .damage = 20,
-    .range = 0,
-    .shape = (int)GEOMETRY_BUFFER_ID::SQUARE, 
-    .size = vec2(100, 100),
-    .gravity_affected = false,
-    .max_cooldown = 0,
-    .current_cooldown = 0
+    false, // chosen
+    0, // speed
+    20, // damage
+    0, // range    
+    (int)GEOMETRY_BUFFER_ID::SQUARE, // shape
+    vec2(100, 100), // size
+    false, // gravity affected
+    0, // max cooldown
+    0 // curr cooldown
 };
 
 AttackAbility gingerbread_advanced_attack = {
-    .activated = false,
-    .speed = 500,
-    .damage = 50,
-    .range = 500,
-    .shape = (int)GEOMETRY_BUFFER_ID::SQUARE, 
-    .size = vec2(100, 50),
-    .gravity_affected = true,
-    .max_cooldown = 2,
-    .current_cooldown = 0
+    false, // chosen
+    500, // speed
+    50, // damage
+    500, // range
+    (int)GEOMETRY_BUFFER_ID::SQUARE, // shape
+    vec2(100, 50), // size
+    true, // gravity affected
+    2, // max cooldown
+    0 // curr cooldown
 };
 
 BuffAbility gingerbread_heal_buff = {
-    .movement_speed_delta = 0,
-    .health_delta = 20,
-    .max_cooldown = 3,
-    .current_cooldown = 0
+    0, // speed delta
+    20, // health delta
+    3, // max cooldown
+    0 // curr cooldown
 };
 
 // Gumball used for enemy, at the moment, enemy only uses melee attack (the basic attack active hardcoded to true)
 AttackAbility gumball_basic_attack = {
-    .activated = false,
-    .speed = 0,
-    .damage = 20,
-    .range = 0,
-    .shape = (int)GEOMETRY_BUFFER_ID::SQUARE, 
-    .size = vec2(100, 100),
-    .gravity_affected = false,
-    .max_cooldown = 0,
-    .current_cooldown = 0
+    false,
+    0,
+    20,
+    0,
+    (int)GEOMETRY_BUFFER_ID::SQUARE, 
+    vec2(100, 100),
+    false,
+    0,
+    0
 };
 
 AttackAbility gumball_advanced_attack = {
-    .activated = false,
-    .speed = 500,
-    .damage = 40,
-    .range = 100,
-    .shape = (int)GEOMETRY_BUFFER_ID::SQUARE, 
-    .size = vec2(100, 100),
-    .gravity_affected = false,
-    .max_cooldown = 2,
-    .current_cooldown = 0
+    false,
+    500,
+    40,
+    100,
+    (int)GEOMETRY_BUFFER_ID::SQUARE, 
+    vec2(100, 100),
+    false,
+    2,
+    0
 };
