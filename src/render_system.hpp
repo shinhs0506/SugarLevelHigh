1322febdde4548143b8dfc466c2c081f85d2570b
Karen <kpl.lau@gmail.com>
2022-04-12 15:44:37 -0700

    update credit for assets

--- src/render_system.hpp
+++ src/render_system.hpp
@@ -113,9 +113,9 @@ class RenderSystem {
 	textures_path("level_2_start.png"),
 	textures_path("level_3_start.png"),
 	textures_path("level_4_start.png"),
-	textures_path("button_melee.png"),
-	textures_path("button_advanced.png"),
-	textures_path("button_heal.png"),
+	textures_path("button_melee.png"), // edited https://paperhatlizard.itch.io/cryos-mini-gui
+	textures_path("button_advanced.png"), // edited https://paperhatlizard.itch.io/cryos-mini-gui
+	textures_path("button_heal.png"), // edited https://paperhatlizard.itch.io/cryos-mini-gui
 	textures_path("level_1_lost.png"),
 	textures_path("level_2_lost.png"),
 	textures_path("level_3_lost.png"),
